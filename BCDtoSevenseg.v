4abb3a55ce21b92f06a79fe3eabe34812849e821
KRVPerera <rukshan.viduranga@gmail.com>
2016-10-04 10:34:11 +0530

    added .v files

--- BCDtoSevenseg.v
+++ BCDtoSevenseg.v
@@ -18,24 +18,24 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module BCDtoSevenseg(bcd, seg);
+module BCDtoSevenseg(bcd, segment);
 	 input [3:0] bcd;
-    output reg [6:0] seg;
+    output reg [6:0] segment;
 	 
 	 always @(bcd)
     begin
         case (bcd) //case statement
-            0 : seg = 7'b0000001;
-            1 : seg = 7'b1001111;
-            2 : seg = 7'b0010010;
-            3 : seg = 7'b0000110;
-            4 : seg = 7'b1001100;
-            5 : seg = 7'b0100100;
-            6 : seg = 7'b0100000;
-            7 : seg = 7'b0001111;
-            8 : seg = 7'b0000000;
-            9 : seg = 7'b0000100; 
-            default : seg = 7'b1111111; 
+            0 : segment = 7'b0000001;
+            1 : segment = 7'b1001111;
+            2 : segment = 7'b0010010;
+            3 : segment = 7'b0000110;
+            4 : segment = 7'b1001100;
+            5 : segment = 7'b0100100;
+            6 : segment = 7'b0100000;
+            7 : segment = 7'b0001111;
+            8 : segment = 7'b0000000;
+            9 : segment = 7'b0000100; 
+            default : segment = 7'b1111111; 
         endcase
     end
 endmodule
--- top.v
+++ top.v
@@ -18,25 +18,17 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module top(clk, bcdl, ssego, an0);
+module top(clk, bcdl, segment, an0);
 	 input clk;
-	 input [7:0] bcdl;
-	 output reg [6:0] ssego;
+	 input wire [7:0] bcdl;
+	 output reg [6:0] segment;
 	 output reg [3:0] an0;
 		
+	 reg [16:0]count;
 	 
+	 wire [6:0] sseg2; 
+	 wire [6:0] sseg3;
 	
-	reg [16:0]count;
-	 
-	reg [6:0] sseg0; 
-	reg [6:0] sseg1; 
-	wire [6:0] sseg2; 
-	wire [6:0] sseg3;
-	
-	assign sseg0 = sseg2;
-	assign sseg1 = sseg3;
-
-	 
 	always @ (posedge clk)
 	begin
 	if (count >= 49999)
@@ -47,56 +39,32 @@ module top(clk, bcdl, ssego, an0);
 	
 	always @ (*)
 	begin
-	case(count[16:15]) //using only the 2 MSB's of the counter 
+	case(count[15]) 
     
-   2'b00 :  //When the 2 MSB's are 00 enable the fourth display
+   1'b0 :  
     begin
-	  ssego = sseg0;
+	  segment = sseg2;
      an0 = 4'b1110;
     end
     
-   2'b01:  //When the 2 MSB's are 01 enable the third display
+   1'b1:  
     begin
-	 ssego = sseg1;
+	 segment = sseg3;
      an0 = 4'b1101;
     end
-    
-   2'b10:  //When the 2 MSB's are 10 enable the second display
-    begin
-	  ssego = sseg0;
-     an0 = 4'b1011;
-    end
-     
-   2'b11:  //When the 2 MSB's are 11 enable the first display
-    begin
-	  ssego = sseg1;
-     an0 = 4'b0111;
-    end
-		endcase
+  
+	endcase
 	end
 	
 	
 	BCDtoSevenseg bcds0 (
 		.bcd(bcdl[3:0]),
-		.seg(sseg2)
+		.segment(sseg2)
 	);
 	
 	BCDtoSevenseg bcds1 (
 		.bcd(bcdl[7:4]),
-		.seg(sseg3)
+		.segment(sseg3)
 	);
 	
-//		BCDtoSevenseg bcds2(
-//		.bcd(bcdl[3:0]),
-//		.segment(sseg3)
-//	);
-//
-//
-//
-//	BCDtoSevenseg bcds3 (
-//		.bcd(bcdl[7:4]),
-//		.segment(sseg4)
-//	);
-
-
 endmodule
