f7f0e643f8bd9cb4f03a4b676309e9b804a3fcaf
KRVPerera <rukshan.viduranga@gmail.com>
2016-10-06 13:36:23 +0530

    removed the carry in pin

--- bcd_digit.v
+++ bcd_digit.v
@@ -18,19 +18,20 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module bcd_digit(clk, c_in, reset, digit, c_out);
+module bcd_digit(clk, reset, digit, c_out);
 	
 	input clk;
-   input c_in;
+   //input c_in;
    input reset;
    output reg [3:0] digit;
    output reg c_out = 0;
 
 	initial digit = 0;
 	
-	always @(posedge clk)
+	always @(posedge clk or posedge reset)
 	begin
-	 c_out <= (c_in && digit == 4'b1000) || (digit == 4'b1001);
+	 	//c_out <= (c_in && digit == 4'b1000) || (digit == 4'b1001);
+		c_out <= (digit == 4'b1000);
 
     if (reset)
 		begin
